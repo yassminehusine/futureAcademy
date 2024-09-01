0b7103e8ba191898aedbb02360c6a799cbc897b1
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-01 21:50:12 +0300

    Update materialController.php

--- app/Http/Controllers/materialController.php
+++ app/Http/Controllers/materialController.php
@@ -1,41 +1,59 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Http\Requests\MaterialRequest;
+use App\DTO\materialDTO;
+use App\Models\materialModel;
+use App\Repository\interface\ImaterialRepository;
+use App\Repository\interface\IcoursesRepository;
+use DB;
 use RealRashid\SweetAlert\Facades\Alert;
+
 class materialController extends Controller
 {
+    protected $courseRepository;
+    protected $departmentRepository;
+    protected $userRepository;
+    public function __construct(IcoursesRepository $courseRepository, ImaterialRepository $materialRepository){
+        $this->middleware(['Admin','doctors']);
+        $this->courseRepository = $courseRepository;
+        $this->materialRepository = $materialRepository;
+    }
     /**
      * Display a listing of the resource.
-     */
+     **/
     public function index()
     {
-        //
+        $materials = $this->materialRepository->getAll();
+        materialModel::with('course')->get();
+       return view('layouts.dashboard.materials.index',compact('materials'));
     }
-
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $courses = $this->courseRepository->getAll();
+        return view('layouts.dashboard.material.create', compact('courses'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(MaterialRequest $request){
+        // dd($request);
+        $material = materialsDTO::handleInputs($request);
+        // dd($material);
+        $this->materialRepository->create($material);
+        Alert::success('Success Toast','success');
+        return redirect()->route('material.index');   
     }
-
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+       //$material = $this->materialRepository->getById($id);
+       //return view('layouts.dashboard.material.show', compact('material'));
     }
 
     /**
@@ -43,15 +61,22 @@ class materialController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $material = $this->materialRepository->getById($id);
+        $courses = $this->courseRepository->getAll();
+        return view('layouts.dashboard.material.edit', compact('courses','material'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(MaterialRequest $request, string $id)
     {
-        //
+      // Convert the request to a DTO
+      $data = materialDTO::handleInputs($request);
+      $this->materialRepository->update($data, $id);
+      return redirect()->route('material.index')->with('success', 'Course updated successfully');
+        
     }
 
     /**
@@ -59,6 +84,8 @@ class materialController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->materialRepository->delete($id);
+        return redirect()->back();
+
     }
 }
