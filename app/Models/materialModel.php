e80e17d7c00bcafd73f8e672b9abb59028f2f285
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-03 09:48:19 +0300

    Update materialModel.php

--- app/Models/materialModel.php
+++ app/Models/materialModel.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class materialModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'material'
+
+    protected $fillable = [
+    'title',
+    'description',
+    'material_type',
+    'file_path',
+    'video_duration',
+    'file_size',
+    'user_id',
+    'file_format',
+    'course_id',]
 }
