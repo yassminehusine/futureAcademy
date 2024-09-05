f0219c3dd9436bda13c396bfd7c5d0f67cca2216
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-05 15:22:32 +0300

    create submission passed

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -22,7 +22,7 @@ class UserRequest extends FormRequest
             'name' => 'required',
             'image' => 'nullable',
             'role' => 'required',
-            'academic_years' => 'nullable',
+            'academic_level' => 'nullable',
             'GPA' => 'required',
             'phone' => 'required',
             'address' => 'required',
