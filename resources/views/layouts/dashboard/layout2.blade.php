8294d20b9d5cbca947880567d501c883a3b474d9
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-08 23:43:56 +0300

    fix issues 2

--- resources/views/layouts/dashboard/layout2.blade.php
+++ resources/views/layouts/dashboard/layout2.blade.php
@@ -199,7 +199,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="{{route('home')}}" class="brand-link">
         <img src="https://futureacademyedu.com/wp-content/uploads/2023/02/Logo-2048x1491.png" alt="AdminLTE Logo"
           class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Future Academyed</span>
